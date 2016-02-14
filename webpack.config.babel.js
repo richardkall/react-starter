a5e2c14773d7d6588ae36629133ab157dc2dff3b
Richard Käll <richard.kall@me.com>
2016-02-14 14:47:41 +0100

    Setup Webpack

---
+++ webpack.config.babel.js
@@ -0,0 +1,35 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  context: path.resolve(__dirname, './client'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    ...process.env.NODE_ENV === 'production' ? [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ] : []
+  ]
+};
