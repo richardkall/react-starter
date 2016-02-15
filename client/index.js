7b4d0d92fbf9fe227e33d7c9470b3ffa55bba4e2
Richard Käll <richard.kall@me.com>
2016-02-15 19:58:25 +0100

    Setup React Router

--- client/index.js
+++ client/index.js
@@ -1,8 +1,14 @@
+import {Router, Route, browserHistory} from 'react-router';
 import App from './containers/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route
+      component={App}
+      path="/"
+    />
+  </Router>,
   document.getElementById('root')
 );
