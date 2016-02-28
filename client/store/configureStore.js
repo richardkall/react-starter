a97ad57dd56da90405d9a25920f36869d07df4cc
Richard Käll <richard.kall@me.com>
2016-02-28 19:36:02 +0100

    Setup Redux Thunk middleware

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,9 +1,13 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import rootReducer from '../reducers';
+import thunkMiddleware from 'redux-thunk';
 
 export default function configureStore (initialState) {
   const create = typeof window !== 'undefined' && window.devToolsExtension
     ? window.devToolsExtension()(createStore)
     : createStore;
-  return create(rootReducer, initialState);
+
+  const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(create);
+
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
