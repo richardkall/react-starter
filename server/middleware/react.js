f6d42090d85a46d77c10f18805b297640b03294f
Richard Käll <richard.kall@me.com>
2016-12-11 22:25:39 +0100

    Simplify server side routing

--- server/middleware/react.js
+++ server/middleware/react.js
@@ -6,31 +6,29 @@ import { renderToStaticMarkup, renderToString } from 'react-dom/server';
 import App from '../../client/components/App';
 import Html from '../views/Html';
 
-const renderApp = (location, context) => renderToString(
-  <ServerRouter location={location} context={context}>
-    <App />
-  </ServerRouter>,
-);
-
 export default function reactMiddleware(req, res) {
   const assets = require('../../build/static/assets.json'); // eslint-disable-line global-require, import/no-unresolved
   const context = createServerRenderContext();
-  let markup = renderApp(req.url, context);
-  let status = 200;
+
+  const markup = renderToString(
+    <ServerRouter location={req.url} context={context}>
+      <App />
+    </ServerRouter>,
+  );
 
   const { missed, redirect } = context.getResult();
-  const styles = styleSheet.rules().map(rule => rule.cssText).join('\n');
 
-  if (missed) {
-    markup = renderApp(req.url, context);
-    status = 404;
-  } else if (redirect) {
+  if (redirect) {
     return res.redirect(301, redirect.pathname);
   }
+
+  const styles = styleSheet.rules().map(rule => rule.cssText).join('\n');
 
   const html = renderToStaticMarkup(
     <Html assets={assets} markup={markup} styles={styles} />,
   );
 
-  return res.status(status).send(`<!doctype html>${html}`);
+  return res
+    .status(missed ? 404 : 200)
+    .send(`<!doctype html>${html}`);
 }
