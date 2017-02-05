c420b36d3eaaf4d8fa70b75188b542efc39e0321
Richard Käll <richard.kall@me.com>
2017-02-05 15:53:47 +0100

    Minor cleanup

--- server/middleware/webpack.js
+++ server/middleware/webpack.js
@@ -6,8 +6,5 @@ import config from '../../webpack/webpack.client.babel';
 
 const compiler = webpack(config);
 
-export const webpackDevMiddleware = devMiddleware(compiler, {
-  stats: 'minimal',
-});
-
+export const webpackDevMiddleware = devMiddleware(compiler, { stats: 'minimal' });
 export const webpackHotMiddleware = hotMiddleware(compiler);
