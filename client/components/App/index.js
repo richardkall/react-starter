572fe335317e33209c17faf727069b9813ec57dc
Richard Käll <richard.kall@me.com>
2017-03-05 21:32:31 +0100

    Rename pages to routes

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from '../../pages/Home';
-import About from '../../pages/About';
-import NotFound from '../../pages/NotFound';
+import Home from '../../routes/Home';
+import About from '../../routes/About';
+import NotFound from '../../routes/NotFound';
 import Navigation from '../Navigation';
 
 import './global-styles';
